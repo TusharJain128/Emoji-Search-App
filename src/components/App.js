0a3a9b8766c72c0f5c6d5038144cb025d37fc653
TusharJain128 <tusharjain128@gmail.com>
2023-04-30 21:30:06 +0530

    basic emoji search

---
+++ src/components/App.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import EmojiList from './EmojiList.json';
+
+export default function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [sortedList, setSortedList] = useState([])
+
+    useEffect(()=>{
+      setSortedList(EmojiList)
+    },[])
+
+    function handleSearch(e){
+      setSearchQuery(e.target.value)
+
+      if(e.target.value.length==0){
+        setSortedList(EmojiList)
+      }
+      else{
+        let data = EmojiList.filter(x=> x.title.toLowerCase().includes(e.target.value.toLowerCase()))
+        setSortedList(data)
+      }
+    }
+
+  return (
+    <div>
+      <h1>Emoji Search App</h1>
+      <input
+        type="text"
+        placeholder="Search for an emoji..."
+        value={searchQuery}
+        onChange={handleSearch}
+      />
+      <div>
+        {sortedList.map((emoji) => (
+          <div key={Math.random()}>
+            <span>{emoji.symbol}</span> {emoji.title}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
